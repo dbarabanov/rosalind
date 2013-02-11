572e9785a5c4008252c409b751237a4c9234c25f
Dmitri Barabanov <dmitri@brooklynpacket.com>
2013-02-10 20:32:02 -0800

    calling EncodeProtein from main

--- src/github.com/dbarabanov/rosalind/main/main.go
+++ src/github.com/dbarabanov/rosalind/main/main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"fmt"
 	"github.com/dbarabanov/rosalind/problems"
+	"github.com/dbarabanov/rosalind/util"
 )
 
 func main() {
 	fmt.Printf("in main\n")
 	fmt.Printf("%v\n", problems.CountNucleotides("AT"))
-	iter := problems.MakeStringIterator("ATCGT")
+	iter := util.MakeStringIterator("ATCGT")
 	var counts = map[byte]int{}
 	for {
 		letter, ok := iter()
@@ -18,4 +19,8 @@ func main() {
 		counts[letter]++
 	}
 	fmt.Println(counts)
+
+	var rna = "AUGGCCAUGGCGCCCAGAACUGAGAUCAAUAGUACCCGUAUUAACGGGUGA"
+
+	fmt.Println(problems.EncodeProtein(rna))
 }
