a981012df2ee58833d7084ce9bd032b473a98ede
Dmitri Barabanov <dmitri@brooklynpacket.com>
2013-02-13 15:45:30 -0800

    constructing and printing suffix tree in O(n**2)

--- src/github.com/dbarabanov/rosalind/suffix_tree/suffix_tree.go
+++ src/github.com/dbarabanov/rosalind/suffix_tree/suffix_tree.go
@@ -6,7 +6,8 @@ type SuffixTree struct {
 	root *Node
 }
 
-const Terminator = '$'
+//const Terminator = '$'
+const Terminator = "$"
 
 type Node struct {
 	Edges []*Edge
@@ -19,67 +20,90 @@ type Edge struct {
 }
 
 func ConstructSuffixTree(s string) (t *SuffixTree) {
-	fmt.Println("constructing suffix tree from %v", s)
-	st := new(SuffixTree)
-	root := new(Node)
-	text := []int32(s)
+	fmt.Println("constructing suffix tree from", s)
+	root := &Node{nil}
+	st := &SuffixTree{root}
+	text := []int32(s + Terminator)
 	for i := range text {
 		addSuffix(root, text[i:])
 	}
-	st.root = root
-	fmt.Printf("constructed: %v\n", st)
-	fmt.Printf("constructed: %p\n", st)
 	Print(st)
 	return st
 }
 
 func Print(st *SuffixTree) {
-	PrintNode(st.root)
+	fmt.Println("whole tree:")
+	PrintNode(st.root, 0)
+
+	//	DeprPrintNode(st.root)
 }
 
-func PrintNode(node *Node) {
+func PrintNode(node *Node, depth int) {
+	if node == nil {
+		return
+	}
+
+	spaces := string(make([]int32, depth))
+	for i := 0; i < depth; i++ {
+		spaces += " "
+	}
+
+	for _, edge := range node.Edges {
+		fmt.Printf("%v%v\n", spaces, string(edge.Value))
+		PrintNode(edge.End, depth+2)
+	}
+}
+
+func DeprPrintNode(node *Node) {
 	if node == nil {
 		return
 	}
 
 	for _, edge := range node.Edges {
 		fmt.Printf("%v\n", string(edge.Value))
-		PrintNode(edge.End)
-
+		DeprPrintNode(edge.End)
 	}
 }
 
-func AppendEdge(edges []*Edge, newEdge *Edge) []*Edge {
-	m := len(edges)
-	fmt.Println("edges: %v. m: %v", edges, m)
-	if m+1 > cap(edges) { // if necessary, reallocate
+func AppendEdge(node *Node, newEdge *Edge) {
+	m := len(node.Edges)
+	if m+1 > cap(node.Edges) { // if necessary, reallocate
 		// allocate double what's needed, for future growth.
 		newEdges := make([]*Edge, (m+1)*2)
-		copy(newEdges, edges)
-		edges = newEdges
+		copy(newEdges, node.Edges)
+		node.Edges = newEdges
 	}
-	fmt.Println("edges: %v", edges)
-	edges = edges[0 : m+1]
-	//	copy(edges[m:m+1], newEdge)
-	//	edges[m+1] = newEdge
-	edges[m] = newEdge
-	return edges
+	node.Edges = node.Edges[0 : m+1]
+	node.Edges[m] = newEdge
 }
 
 func addSuffix(node *Node, suffix []int32) {
-	if node.Edges == nil {
-		fmt.Println("no edges")
-		node.Edges = make([]*Edge, 1)
-		node.Edges[0] = &Edge{suffix, node, nil}
-	} else {
-		fmt.Println("has edges")
-//		AppendEdge(node.Edges, &Edge{suffix, node, nil})
-        node.Edges = AppendEdge(node.Edges, &Edge{suffix, node, nil})
- 
-		fmt.Println("added: %v", node.Edges)
+	fmt.Printf("adding suffix %v\n", string(suffix))
+	for _, edge := range node.Edges {
+		//		fmt.Printf("%v\n", edge)
+		if edge.Value[0] == suffix[0] {
+			fmt.Printf("matching edge for %v: %v\n", string(suffix), string(edge.Value))
+			for i := range edge.Value {
+				//				fmt.Printf("index: %v. edge.Value: %v\n", i, string(edge.Value[i]))
+				if edge.Value[i] != suffix[i] {
+					insertNode(edge, i, suffix[i:])
+					return
+				}
+			}
+			return
+		}
 	}
+	AppendEdge(node, &Edge{suffix, node, nil})
+}
 
-	fmt.Println(node.Edges)
+func insertNode(edge *Edge, index int, suffix []int32) {
+	fmt.Printf("inserting suffix %v at index %v of %v\n", string(suffix), index, string(edge.Value))
+	firstPart := &Edge{edge.Value[:index], edge.Start, &Node{nil}}
+	AppendEdge(firstPart.End, &Edge{suffix, firstPart.End, &Node{nil}})
+	AppendEdge(firstPart.End, &Edge{edge.Value[index:], firstPart.End, edge.End})
+	edge.End = firstPart.End
+	edge.Value = firstPart.Value
+	DeprPrintNode(edge.Start)
 }
 
 func FindSubstrings(st *SuffixTree, substring string) (indexes []int32) {
