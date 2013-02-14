91fb200a9c82683eac7010405dbb81828847f30a
Dmitri Barabanov <dmitri@brooklynpacket.com>
2013-02-14 12:55:21 -0800

    working FindSubstrings on suffix tree

--- src/github.com/dbarabanov/rosalind/suffix_tree/suffix_tree.go
+++ src/github.com/dbarabanov/rosalind/suffix_tree/suffix_tree.go
@@ -6,26 +6,30 @@ type SuffixTree struct {
 	root *Node
 }
 
-//const Terminator = '$'
 const Terminator = "$"
 
+type NodeData struct {
+	LeafIndex int
+}
+
 type Node struct {
-	Edges []*Edge
+	Edges    []*Edge
+	NodeData *NodeData
 }
 
 type Edge struct {
-	Value []int32
+	Label []rune
 	Start *Node
 	End   *Node
 }
 
 func ConstructSuffixTree(s string) (t *SuffixTree) {
 	fmt.Println("constructing suffix tree from", s)
-	root := &Node{nil}
+	root := &Node{nil, nil}
 	st := &SuffixTree{root}
-	text := []int32(s + Terminator)
+	text := []rune(s + Terminator)
 	for i := range text {
-		addSuffix(root, text[i:])
+		addSuffix(root, text[i:], i)
 	}
 	Print(st)
 	return st
@@ -34,8 +38,6 @@ func ConstructSuffixTree(s string) (t *SuffixTree) {
 func Print(st *SuffixTree) {
 	fmt.Println("whole tree:")
 	PrintNode(st.root, 0)
-
-	//	DeprPrintNode(st.root)
 }
 
 func PrintNode(node *Node, depth int) {
@@ -49,7 +51,7 @@ func PrintNode(node *Node, depth int) {
 	}
 
 	for _, edge := range node.Edges {
-		fmt.Printf("%v%v\n", spaces, string(edge.Value))
+		fmt.Printf("%v%v\n", spaces, string(edge.Label))
 		PrintNode(edge.End, depth+2)
 	}
 }
@@ -60,7 +62,7 @@ func DeprPrintNode(node *Node) {
 	}
 
 	for _, edge := range node.Edges {
-		fmt.Printf("%v\n", string(edge.Value))
+		fmt.Printf("%v\n", string(edge.Label))
 		DeprPrintNode(edge.End)
 	}
 }
@@ -77,36 +79,132 @@ func AppendEdge(node *Node, newEdge *Edge) {
 	node.Edges[m] = newEdge
 }
 
-func addSuffix(node *Node, suffix []int32) {
+func addSuffix(node *Node, suffix []rune, position int) {
 	fmt.Printf("adding suffix %v\n", string(suffix))
 	for _, edge := range node.Edges {
 		//		fmt.Printf("%v\n", edge)
-		if edge.Value[0] == suffix[0] {
-			fmt.Printf("matching edge for %v: %v\n", string(suffix), string(edge.Value))
-			for i := range edge.Value {
-				//				fmt.Printf("index: %v. edge.Value: %v\n", i, string(edge.Value[i]))
-				if edge.Value[i] != suffix[i] {
-					insertNode(edge, i, suffix[i:])
+		if edge.Label[0] == suffix[0] {
+			fmt.Printf("matching edge for %v: %v\n", string(suffix), string(edge.Label))
+			for i := range edge.Label {
+				//				fmt.Printf("index: %v. edge.Label: %v\n", i, string(edge.Label[i]))
+				if edge.Label[i] != suffix[i] {
+					insertNode(edge, i, suffix[i:], position)
 					return
 				}
 			}
 			return
 		}
 	}
-	AppendEdge(node, &Edge{suffix, node, nil})
+	//	AppendEdge(node, &Edge{suffix, node, nil})
+	AppendEdge(node, &Edge{suffix, node, &Node{nil, &NodeData{position}}})
 }
 
-func insertNode(edge *Edge, index int, suffix []int32) {
-	fmt.Printf("inserting suffix %v at index %v of %v\n", string(suffix), index, string(edge.Value))
-	firstPart := &Edge{edge.Value[:index], edge.Start, &Node{nil}}
-	AppendEdge(firstPart.End, &Edge{suffix, firstPart.End, &Node{nil}})
-	AppendEdge(firstPart.End, &Edge{edge.Value[index:], firstPart.End, edge.End})
+func insertNode(edge *Edge, index int, suffix []rune, position int) {
+	fmt.Printf("inserting suffix %v at index %v of %v\n", string(suffix), index, string(edge.Label))
+	firstPart := &Edge{edge.Label[:index], edge.Start, &Node{nil, nil}}
+	AppendEdge(firstPart.End, &Edge{suffix, firstPart.End, &Node{nil, &NodeData{position}}})
+	AppendEdge(firstPart.End, &Edge{edge.Label[index:], firstPart.End, edge.End})
 	edge.End = firstPart.End
-	edge.Value = firstPart.Value
-	DeprPrintNode(edge.Start)
+	edge.Label = firstPart.Label
+	//	DeprPrintNode(edge.Start)
 }
 
-func FindSubstrings(st *SuffixTree, substring string) (indexes []int32) {
-	indexes = make([]int32, 1)
+func FindSubstrings(st *SuffixTree, substring string) (indexes map[int]struct{}) {
+	if st == nil || st.root == nil {
+		return indexes
+	}
+	return findSubstrings(st.root, []rune(substring))
+}
+
+func findSubstrings(node *Node, pattern []rune) (indexes map[int]struct{}) {
+	index := 0
+	var activeEdge *Edge = nil
+	for _, edge := range node.Edges {
+		fmt.Println(string(edge.Label))
+		if edge.Label[0] == pattern[index] {
+			activeEdge = edge
+			break
+		}
+	}
+	if activeEdge != nil {
+		fmt.Printf("activeEdge: %v. index: %v\n", string(activeEdge.Label), index)
+		for _, l := range activeEdge.Label {
+			if index >= len(pattern) {
+				fmt.Println("A")
+				//				TraverseNodes(activeEdge.End, indexes)
+				return collectLeafsBelow(activeEdge.End)
+			}
+			if pattern[index] != l {
+				fmt.Println("B")
+				return indexes
+			}
+			index++
+		}
+		if index >= len(pattern) {
+			return collectLeafsBelow(activeEdge.End)
+		}
+		return findSubstrings(activeEdge.End, pattern[len(activeEdge.Label):])
+	}
+	return indexes
+}
+
+func collectLeafsBelow(node *Node) (indexes map[int]struct{}) {
+	indexes = make(map[int]struct{})
+	ch := Walker(node)
+	for {
+		index, ok := <-ch
+		if !ok {
+			return indexes
+		}
+		indexes[index] = struct{}{}
+	}
+	return indexes
+}
+
+func WalkLeafs(n *Node, ch chan int) {
+	if n == nil {
+		panic("attempting to walk nil node")
+	}
+	if len(n.Edges) <= 0 {
+		ch <- n.NodeData.LeafIndex
+	}
+	for _, edge := range n.Edges {
+		WalkLeafs(edge.End, ch)
+	}
+}
+
+func Walker(n *Node) <-chan int {
+	ch := make(chan int)
+	go func() {
+		WalkLeafs(n, ch)
+		close(ch)
+	}()
+	return ch
+}
+
+func TraverseNodes(node *Node, indexes []int) {
+	fmt.Printf("Traversing node %v\n", node.NodeData)
+	if len(node.Edges) <= 0 {
+		fmt.Printf("Leaf %v\n", node.NodeData)
+		AppendInt(indexes, node.NodeData.LeafIndex)
+		//		return indexes
+	} else {
+		for _, edge := range node.Edges {
+			TraverseNodes(edge.End, indexes)
+		}
+		//		return indexes
+	}
+}
+
+func AppendInt(indexes []int, index int) []int {
+	m := len(indexes)
+	if m+1 > cap(indexes) { // if necessary, reallocate
+		// allocate double what's needed, for future growth.
+		newIndexes := make([]int, (m+1)*2)
+		copy(newIndexes, indexes)
+		indexes = newIndexes
+	}
+	indexes = indexes[0 : m+1]
+	indexes[m] = index
 	return indexes
 }
--- src/github.com/dbarabanov/rosalind/suffix_tree/suffix_tree_test.go
+++ src/github.com/dbarabanov/rosalind/suffix_tree/suffix_tree_test.go
@@ -6,6 +6,21 @@ import (
 )
 
 func TestSuffixTree(t *testing.T) {
-	st := ConstructSuffixTree("banana")
-	FindSubstrings(st, "an")
+	text := "banana"
+pattern := "an"
+//	pattern := "na"
+	st := ConstructSuffixTree(text)
+	//	indexes := FindSubstrings(st, "an")
+	//	out := []int{1, 3}
+	//out := []int{2, 4}
+//	out := map[int]struct{}{2:struct{}{}, 4:struct{}{}}
+	out := map[int]struct{}{1:struct{}{}, 3:struct{}{}}
+	x := FindSubstrings(st, pattern)
+	for i := range out {
+		if len(x) <= 0 || x[i] != out[i] {
+			t.Errorf("FindSubstrings %v in %v failed. want %v. found %v. diff in %v",
+				pattern, text, out, x, i)
+			return
+		}
+	}
 }
